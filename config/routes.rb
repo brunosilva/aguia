a5ccfdda5c8a8c3296ffff08e26b4467db1a3ad8
Bruno Silva <brs.rsilva@gmail.com>
2025-07-07 11:19:24 -0300

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, only: %i[sessions registrations passwords]
 
   namespace :admin do
+    resources :users
     resources :abouts
     resources :basics
     resources :logos
